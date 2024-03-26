767eb59a4a65f9d3b4be900e0d46cd9cba4d4e21
amir mohammad firoozvand <firoozvand655@gmail.com>
2024-03-26 13:31:41 +0330

    end

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+﻿let $ = document;
+let bulbImg = $.getElementById('bulb')
+let btn = $.getElementById('btn')
+
+let bulbFlag = false
+
+function turnOffOrOn() {
+    // alert('Turn On/Off')
+
+    if (bulbFlag) {
+        bulbImg.setAttribute('src', 'assets/images/bulboff.gif')
+        btn.innerHTML = 'Turn On'
+        bulbFlag = false
+    } else {
+        bulbImg.setAttribute('src', 'assets/images/bulbon.gif')
+        btn.innerHTML = 'Turn Off'
+        bulbFlag = true
+    }
+}
